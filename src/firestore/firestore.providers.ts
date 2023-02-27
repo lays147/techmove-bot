7630dcc5e78daca2b935b5c9af03c02dbbc9aa8f
Lays Rodrigues <laysrodriguessilva@gmail.com>
2023-02-27 16:46:32 -0300

    feat: remove stores document
    Signed-off-by: Lays Rodrigues <laysrodriguessilva@gmail.com>

--- src/firestore/firestore.providers.ts
+++ src/firestore/firestore.providers.ts
@@ -1,9 +1,7 @@
-import { ScoresCollection } from '@app/scores/documents/scores.documents';
 import { UsersCollection } from '@app/users/collections/users.collection';
 
 export const FirestoreDatabaseProvider = 'firestoredb';
 export const FirestoreOptionsProvider = 'firestoreOptions';
 export const FirestoreCollectionProviders: string[] = [
-    ScoresCollection.collectionName,
     UsersCollection.collectionName,
 ];
--- src/scores/documents/scores.documents.ts
+++
@@ -1,7 +0,0 @@
-import { UserDto } from '@app/users/dto/user.dto';
-
-const MONTH = new Date().toLocaleString('default', { month: 'long' });
-
-export class ScoresCollection extends UserDto {
-    static collectionName = `scores-${MONTH.toLowerCase()}`;
-}
