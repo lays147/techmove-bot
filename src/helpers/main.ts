070d0acda3bf7a9368d9f4c51a7c7cc159829fba
Lays Rodrigues <laysrodriguessilva@gmail.com>
2023-04-11 22:41:12 -0300

    feat: add helper function to convert str to pt-br date
    Signed-off-by: Lays Rodrigues <laysrodriguessilva@gmail.com>

--- src/helpers/main.ts
+++ src/helpers/main.ts
@@ -3,3 +3,7 @@ import { MATCH_UNTIL_SPACE } from '@app/constants';
 export const cleanUpCommand = (command: string): string[] => {
     return command.replace(MATCH_UNTIL_SPACE, '').replace(' ', '').split(',');
 };
+
+export const stringToPtBrDate = (date: string): Date => {
+    return new Date(date.split('-').reverse().join('/') + ' 00:00:00');
+};
